5ddd8a5396b41fff57b73bf909b1fb68039dcc69
Alex <nypels.alexandre@outlook.com>
2021-11-28 21:35:50 +0000

    adding function to display user and house pick

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,61 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
-    }
+let UserPick;
+let HousePick;
+
+let getUserPick = () => {
+  document.querySelectorAll('.symbol').forEach(symbol => {
+    symbol.addEventListener('click', (e) => {
+      UserPick = e.path[1].id
+      startGame(UserPick)
+    })
+  })
 }
+
+let displayNot = (id) => document.getElementById(id).classList.add("display-not")
+let display = (id) => document.getElementById(id).classList.remove("display-not")
+
+let displayUserPick = (symbol) => {
+  document.getElementById("userPickSymbol").src = `assets/images/icon-${symbol}.svg`
+  document.getElementById("userPickContainer").classList.add(symbol)
+}
+
+let displayHousePick = (symbol) => {
+  symbol = "rock"
+  displayNot("countdown")
+  display("HousePickSymbol")
+  document.getElementById("housePickContainer").classList.remove("empty-symbol")
+  document.getElementById("housePickContainer").classList.add(symbol)
+  document.getElementById("HousePickSymbol").src = `assets/images/icon-${symbol}.svg`
+
+}
+
+let countdown = (count) => {
+  let timeleft = count
+  setInterval(() => {
+    if(timeleft < 0) {
+      clearInterval(countdown)
+    }else{
+      document.getElementById("countdown").innerHTML = timeleft
+      timeleft -= 1
+    }
+  }, 700)
+}
+
+
+let startGame = (symbol) => {
+  // 1- check symbol 
+  // 2- Remove step 1 content and display step 2 in index.html
+  displayNot("step-one")
+  display("step-two")
+  displayUserPick(symbol)
+  //when countdown finished display the house pick
+  countdown(3)
+  setTimeout(() => {
+    displayHousePick()
+  }, 3300);
+
+  
+}
+
+getUserPick()
+
 
