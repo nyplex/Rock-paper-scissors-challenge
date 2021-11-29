5a7c92ceaf18c11d0e9b3d72252b59bb83bd292f
Alex <nypels.alexandre@outlook.com>
2021-11-29 14:30:53 +0000

    adding increaseScore function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 let UserPick;
-let HousePick;
+let symbols = ['rock', 'paper', 'scissors'];
+let HousePick = symbols[Math.floor(Math.random() * symbols.length)]
+let score = 0;
+
 
 
 let getUserPick = () => {
@@ -11,15 +14,6 @@ let getUserPick = () => {
   })
 }
 
-//get House Pick function
-let getHousePick = () => {
-  let symbols = ['rock', 'paper', 'scissors']
-  let symbol = arr => arr[Math.floor(Math.random() * arr.length)];
-  let HousePick = symbol(symbols)
-  return HousePick
-  
-}
-
 let displayNot = (id) => document.getElementById(id).classList.add("display-not")
 let display = (id) => document.getElementById(id).classList.remove("display-not")
 
@@ -29,12 +23,11 @@ let displayUserPick = (symbol) => {
 }
 
 let displayHousePick = () => {
-  symbol = getHousePick()
   displayNot("countdown")
   display("HousePickSymbol")
   document.getElementById("housePickContainer").classList.remove("empty-symbol")
-  document.getElementById("housePickContainer").classList.add(symbol)
-  document.getElementById("HousePickSymbol").src = `assets/images/icon-${symbol}.svg`
+  document.getElementById("housePickContainer").classList.add(HousePick)
+  document.getElementById("HousePickSymbol").src = `assets/images/icon-${HousePick}.svg`
 }
 
 let countdown = (count) => {
@@ -49,21 +42,52 @@ let countdown = (count) => {
   }, 700)
 }
 
+let whoWin = (userPick, housePick) => {
+  if(userPick == housePick) return "equal"
+  else if(userPick == "paper" && housePick == "rock") return "win"
+  else if(userPick == "paper" && housePick == "scissors") return "lost"
+  else if(userPick == "rock" && housePick == "scissors") return "win"
+  else if(userPick == "rock" && housePick == "paper") return "lost"
+  else if(userPick == "scissors" && housePick == "paper") return "win"
+  else if(userPick == "scissors" && housePick == "rock") return "lost"
+}
+
+let IncreaseScore = (status) => {
+  if(status == "win") score += 1
+  return score
+}
+
+let DisplayWhoWin = () => {
+  let content;
+  if(whoWin(UserPick, HousePick) == "equal" || whoWin(UserPick, HousePick) == "lost"){
+    content = "No luck this time!"
+  }else {
+    content = "You Won!"
+  }
+  IncreaseScore(whoWin(UserPick, HousePick))
+  document.getElementById("whoWon").innerHTML = content
+  setTimeout(() => {
+    document.getElementById("userPickContainer").classList.add("step-two-left-open")
+    document.getElementById("housePickContainer").classList.add("step-two-right-open")
+    document.getElementById("whoWonContainer").classList.remove("display-not")
+  }, 4000);
+  setTimeout(() => {
+    document.getElementById("whoWonContainer").style.animationName = "whoWon"
+    document.getElementById("scoreSpan").innerHTML = score
+  }, 4100);
+}
+
 
 let startGame = (symbol) => {
-  // 1- check symbol 
-  // 2- Remove step 1 content and display step 2 in index.html
   displayNot("step-one")
   display("step-two")
   displayUserPick(symbol)
-  //when countdown finished display the house pick
   countdown(3)
   setTimeout(() => {
     displayHousePick()
   }, 3300);
+  DisplayWhoWin()
 
-  console.log("user: " + symbol)
-  console.log("house: " + getHousePick(symbol));
 
   
 }
